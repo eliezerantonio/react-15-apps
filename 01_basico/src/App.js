4831f7164e57ff0c27d4b68ce32e1bd2a466c218
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-29 12:46:01 +0100

    agora

---
+++ 01_basico/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+const App = () => 
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+
+export default App;
---
+++ 01_basico/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div clasName>
+            <h1>Loja virtual</h1>
+        </div>
+    )
+}
+
+export default Header
