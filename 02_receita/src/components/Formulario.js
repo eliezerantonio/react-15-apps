638a7b448c8f7cae8eeeddec8734e8b8455e1f54
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-10-08 01:08:44 +0100

    handleChange

--- 02_receita/src/components/Formulario.js
+++ 02_receita/src/components/Formulario.js
@@ -1,6 +1,19 @@
 import React, { Fragment } from "react";
 
 const Formulario = () => {
+  //criar stado da reeita
+  const [receita, setReceita] = React.useState({
+    mascote: "",
+    proprietario: "",
+    data: "",
+    hora: "",
+    sintomas: "",
+  });
+  //funcao que se secuta cada vez que o usauiro escreve algo
+
+  const handleChange = () => {
+    console.log("escrevendo");
+  };
   return (
     <Fragment>
       <h2>Criar receita</h2>
@@ -11,6 +24,7 @@ const Formulario = () => {
           name="mascote"
           className="u-full-width"
           placeholder="Nome da mascote"
+          onChange={handleChange}
         />
 
         <label>Nome Dono</label>
@@ -19,18 +33,34 @@ const Formulario = () => {
           name="proprietario"
           className="u-full-width"
           placeholder="Nome do propp"
+          onChange={handleChange}
         />
 
         <label>Data</label>
 
-        <input type="date" name="data" className="u-full-width" />
+        <input
+          type="date"
+          name="data"
+          className="u-full-width"
+          onChange={handleChange}
+        />
 
         <label>Hora</label>
 
-        <input type="time" name="hora" className="u-full-width" />
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={handleChange}
+        />
 
         <label>Sintomas</label>
-        <textarea className="u-full-width" name="Sintomas"></textarea>
+
+        <textarea
+          className="u-full-width"
+          name="Sintomas"
+          onChange={handleChange}
+        ></textarea>
 
         <button type="submit" className="u-full-width button-primary">
           Passar receita
