6a614b01874121ae6a99197ad6e8f999fb6482d2
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-10-30 01:55:24 +0100

    formulario de pais

--- 06_clima/src/components/Formulario.jsx
+++ 06_clima/src/components/Formulario.jsx
@@ -7,6 +7,14 @@ const Formulario = () => {
         <input type="text" className="form-control" name="cidade" id="cidade" />
         <label>Cidade: </label>
       </div>
+
+      <div className="input-field col s12">
+        <select name="pais">
+          <option value="">-- Selecione um pais --</option>
+        </select>
+
+        <label>Pais</label>
+      </div>
     </form>
   );
 };
