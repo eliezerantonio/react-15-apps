4badd2e1b44326c9e9e72d67829e34210c8af162
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-11-13 15:28:42 +0100

    exibindo receitas

--- 02_receita/src/App.js
+++ 02_receita/src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 
 import Formulario from "./components/Formulario";
+import Receita from "./components/Receita";
 
 const App = () => {
   //lista de receitas
@@ -11,6 +12,14 @@ const App = () => {
     atualizarReceita([...receitas, receita]);
   };
 
+  const eliminarReceita = (id) => {
+    const novasReceitas = receitas.filter((receita) => receita.id !== id);
+
+    atualizarReceita(novasReceitas);
+  };
+
+  const titulo =
+    receitas.length === 0 ? "Sem receitas" : "Administra tuas receitas";
   return (
     <Fragment>
       <h1>Administrador de Pacientes</h1>
@@ -20,7 +29,17 @@ const App = () => {
           <div className="one-half column">
             <Formulario criarReceita={criarReceita} />
           </div>
-          <div className="one-half column">2</div>
+          <div className="one-half column">
+            <h2>{titulo}</h2>
+
+            {receitas.map((receita) => (
+              <Receita
+                key={receita.id}
+                receita={receita}
+                eliminarReceita={eliminarReceita}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </Fragment>
--- 02_receita/src/components/Receita.jsx
+++ 02_receita/src/components/Receita.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
 
-const Receita = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Receita = ({ receita, eliminarReceita }) => {
+  return (
+    <div className="cita">
+      <p>
+        Mascote:
+        <span> {receita.mascote} </span>
+      </p>{" "}
+      <p>
+        Dono:
+        <span> {receita.proprietario} </span>
+      </p>{" "}
+      <p>
+        Data:
+        <span> {receita.data} </span>
+      </p>{" "}
+      <p>
+        Hora:
+        <span> {receita.hora} </span>
+      </p>{" "}
+      <p>
+        Sintomas:
+        <span> {receita.sintomas} </span>
+      </p>
+      <button
+        className="button eliminar u-full-width"
+        onClick={() => eliminarReceita(receita.id)}
+      >
+        Eliminar &times;
+      </button>
+    </div>
+  );
+};
 
-export default Receita
+export default Receita;
