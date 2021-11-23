128616d07b1d80ec678c1bcb6750336a6ea5e265
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-11-24 00:07:41 +0100

    mover scool para cima apos mudar de pagina

--- 09_buscador_imagens/src/App.js
+++ 09_buscador_imagens/src/App.js
@@ -2,12 +2,11 @@ import React from "react";
 import Formulario from "./components/Formulario";
 import ListaImages from "./components/ListaImages";
 
-
 const App = () => {
   const [search, setSearch] = React.useState("");
   const [images, setImages] = React.useState([]);
-  
-
+  const [actualPage, setActualPage] = React.useState(1);
+  const [totalPages, setTotalPages] = React.useState(1);
 
   React.useEffect(() => {
     const consultAPI = async () => {
@@ -16,15 +15,35 @@ const App = () => {
 
       const key = "24488788-982acf07953acda6a4845eee0";
 
-      const url = `https://pixabay.com/api/?key=${key}&q=${search}&per_page=${imagesForPages}`;
+      const url = `https://pixabay.com/api/?key=${key}&q=${search}&per_page=${imagesForPages}&page=${actualPage}`;
 
       const response = await fetch(url);
 
       const result = await response.json();
       setImages(result.hits);
+
+      //calcular total de pagins
+      const calculateTotalPages = Math.ceil(result.totalHits / imagesForPages);
+      setTotalPages(calculateTotalPages);
+
+      //mover scool para cima apos mudar de pagina
     };
     consultAPI();
-  }, [search]);
+  }, [actualPage, search]);
+
+  //definir pagina and pag
+  const previousPage = () => {
+    const newActualPage = actualPage - 1;
+    if (newActualPage === 0) return;
+
+    setActualPage(newActualPage);
+  };
+  const nextPage = () => {
+    const newActualPage = actualPage + 1;
+    if (newActualPage > totalPages) return;
+
+    setActualPage(newActualPage);
+  };
   return (
     <div className="container">
       <div className="jumbotron">
@@ -32,8 +51,21 @@ const App = () => {
         <Formulario setSearch={setSearch} />
       </div>
       <div className="row justify-content-center">
-      <ListaImages images={images}/>
-      
+        <ListaImages images={images} />
+        {actualPage === 1 ? null : (
+          <button
+            type="button"
+            className="btn btn-info mr-1"
+            onClick={previousPage}
+          >
+            &laquo; Anterior
+          </button>
+        )}
+        {actualPage === totalPages ? null : (
+          <button type="button" className="btn btn-info" onClick={nextPage}>
+            &raquo; Seguinte
+          </button>
+        )}
       </div>
     </div>
   );
--- 09_buscador_imagens/src/components/Image.jsx
+++ 09_buscador_imagens/src/components/Image.jsx
@@ -7,6 +7,22 @@ const Image = ({ image }) => {
     <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
       <div className="card">
         <img src={previewURL} alt={tags} className="card-img-top" />
+
+        <div className="card-body">
+          <p className="card-text">{likes} Gostos</p>
+          <p className="card-text">{views} Vistas</p>
+        </div>
+
+        <div className="card-footer">
+          <a
+            href={largeImageURL}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-primary btn-block"
+          >
+            Ver Imagem
+          </a>
+        </div>
       </div>
       <p>Imagem</p>
     </div>
