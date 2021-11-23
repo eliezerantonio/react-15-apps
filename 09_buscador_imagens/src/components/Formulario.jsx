e5cf0acde0c93335519296a88ca5122e4b44b4ec
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-11-23 22:57:58 +0100

    mostendo error

--- 09_buscador_imagens/src/components/Formulario.jsx
+++ 09_buscador_imagens/src/components/Formulario.jsx
@@ -1,14 +1,31 @@
 import React from "react";
+import Error from "./Error";
 
 const Formulario = () => {
+  const [termino, saveTermino] = React.useState("");
+  const [error, setError] = React.useState(false);
+
+  const searchImages = (e) => {
+    e.preventDefault();
+    //validar
+    if (termino.trim() === "") {
+      setError(true);
+
+      return;
+    }
+
+    setError(false);
+    //enviar  o termino do bsuca ao componente principal
+  };
   return (
-    <form>
+    <form onSubmit={searchImages}>
       <div className="row">
         <div className="form-group col-md-8">
           <input
             type="text"
             className="form-control form-control-lg"
             placeholder="Busca uma imagem"
+            onChange={(e) => saveTermino(e.target.value)}
           />
         </div>{" "}
         <div className="form-group col-md-4">
@@ -19,6 +36,7 @@ const Formulario = () => {
           />
         </div>
       </div>
+      {error ? <Error message="Adicione uma palavra de busca" /> : null}
     </form>
   );
 };
