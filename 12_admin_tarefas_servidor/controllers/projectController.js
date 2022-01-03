99c972959f841a8076203d586a529c2bfbf4bac9
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-01-03 01:46:25 +0100

    atualizando

--- 12_admin_tarefas_servidor/controllers/projectController.js
+++ 12_admin_tarefas_servidor/controllers/projectController.js
@@ -1,6 +1,12 @@
 const Project = require("../models/Project");
-
+const { validationResult } = require("express-validator");
 exports.createProject = async (req, res) => {
+  //revisar se ha errors
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   try {
     //criar novo project
 
@@ -12,5 +18,44 @@ exports.createProject = async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).send("Errordo servidor");
+  }
+};
+
+//obter toddos project do usuario logado
+
+exports.findAllProjects = async (req, res) => {
+  try {
+    const projects = await Project.find({ creator: req.user.id });
+
+    res.json({ projects });
+  } catch (error) {
+    console.lgo(error);
+    res.status(500).send("Ocorreu um erro");
+  }
+};
+
+//atualizar perojecto
+
+exports.updateProject = async (req, res) => {
+  //revisar se ha errors
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { name } = req.body;
+
+  const newProject = {};
+  if (name) {
+    newProject.name = name;
+  }
+  try {
+    //revisar id
+    //reviar se o projecto existe
+    //verificar criado do projecto
+    //atualizar
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error no servidor");
   }
 };
--- 12_admin_tarefas_servidor/routes/projects.js
+++ 12_admin_tarefas_servidor/routes/projects.js
@@ -1,9 +1,25 @@
 const express = require("express");
-
+const { check } = require("express-validator");
 const router = express.Router();
 const projectController = require("../controllers/projectController");
 const auth = require("../middleware/auth");
 //criar projecto
-router.post("/", auth, projectController.createProject);
+router.post(
+  "/",
+  auth,
+  [check("name", "Informe o nome do projecto")],
+  projectController.createProject
+);
+
+//buscar todos projectos
+router.get("/", auth, projectController.findAllProjects);
+
+//atualizar projecto  projectos
+router.put(
+  "/:id",
+  auth,
+  [check("name", "Informe o nome do projecto")],
+  projectController.updateProject
+);
 
 module.exports = router;
