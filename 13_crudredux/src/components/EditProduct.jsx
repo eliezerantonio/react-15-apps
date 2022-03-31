9cc0ecb8ea7410bff4e4a77a4abfa48594fd32a3
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-03-31 01:58:25 +0100

    editar produtos

--- 13_crudredux/src/components/EditProduct.jsx
+++ 13_crudredux/src/components/EditProduct.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
 
 const EditProduct = () => {
   return (
-    <div>EditProduct</div>
-  )
-}
+    <div className="row justify-content-center my-5">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">
+              Editar Produto
+            </h2>
+            <form>
+              <div className="form-group">
+                <label>Nome </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Nome Produto"
+                  value="name"
+                />{" "}
+                <label>Price</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Preco Produto"
+                  value="price"
+                />
+                <button
+                  type="submit"
+                  className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+                >
+                  Salvar alteracoes
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default EditProduct
+export default EditProduct;
