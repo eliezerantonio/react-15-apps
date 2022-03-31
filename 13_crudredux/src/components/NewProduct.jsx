1b7d681979fa0b87fd9248ed3276cf15b74eb7dd
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-03-31 01:56:24 +0100

    lista de produtos e tela de produtos

--- 13_crudredux/src/components/NewProduct.jsx
+++ 13_crudredux/src/components/NewProduct.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
 
 const NewProduct = () => {
   return (
-    <div>NewProduct</div>
-  )
-}
+    <div className="row justify-content-center my-5">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">
+              Adicionar Produto
+            </h2>
+            <form>
+              <div className="form-group">
+                <label>Nome </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Nome Produto"
+                  value="name"
+                />{" "}
+                <label>Price</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Preco Produto"
+                  value="price"
+                />
+                <button
+                  type="submit"
+                  className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+                >
+                  Adicionar
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default NewProduct
+export default NewProduct;
--- 13_crudredux/src/components/Products.jsx
+++ 13_crudredux/src/components/Products.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <Fragment>
+      <h2 className="text-center my-5 ">Lista de produtos</h2>
+
+      <table className="table table-striped">
+        <thead className="bg-primary table-dark">
+          <tr>
+            <th scope="row">Nome</th>
+            <th scope="row">Preco</th>
+            <th scope="row">Actions</th>
+          </tr>
+          <tbody></tbody>
+        </thead>
+      </table>
+    </Fragment>
+  );
 };
 
 export default Products;
