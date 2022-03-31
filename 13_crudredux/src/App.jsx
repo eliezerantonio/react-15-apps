39cfafbb67c777d5db2754caa2275c2a8386e31e
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-03-31 02:32:51 +0100

    add redux

--- 13_crudredux/src/App.jsx
+++ 13_crudredux/src/App.jsx
@@ -5,17 +5,23 @@ import Header from "./components/Header";
 import NewProduct from "./components/NewProduct";
 import Products from "./components/Products";
 
+//Redux
+import { Provider } from "react-redux";
+import store from "./store";
 const App = () => {
   return (
     <Router>
-      <Header />
-      <div className="container">
-        <Routes>
-          <Route exact path="/" element={<Products />} />
-          <Route exact path="/products/new" element={<NewProduct />} />
-          <Route exat path="/products/edit/:id" element={<EditProduct />} />
-        </Routes>
-      </div>
+      <Provider store={store}>
+        {" "}
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Products />} />
+            <Route exact path="/products/new" element={<NewProduct />} />
+            <Route exat path="/products/edit/:id" element={<EditProduct />} />
+          </Routes>
+        </div>
+      </Provider>
     </Router>
   );
 };
