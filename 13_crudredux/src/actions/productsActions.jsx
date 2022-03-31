79515be789251b8e3fa36479255242ae2876ffef
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-03-31 02:38:55 +0100

    init actitons

---
+++ 13_crudredux/src/actions/productsActions.jsx
@@ -0,0 +1,6 @@
+//funcoes que mudam o estado
+
+import { ADD_PRODUCT, ADD_PRODUCT_SUCCESS, ADD_PRODUCT_ERROR } from "../types";
+
+
+
--- 13_crudredux/src/reducers/productsReducer.jsx
+++ 13_crudredux/src/reducers/productsReducer.jsx
@@ -1,3 +1,5 @@
+import{   ADD_PRODUCT,ADD_PRODUCT_SUCCESS,ADD_PRODUCT_ERROR}from '../types'
+
 //cada reducer tem seu proprio estado
 
 const initalState = {
