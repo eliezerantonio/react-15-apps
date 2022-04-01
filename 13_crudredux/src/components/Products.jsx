ac4378e4ba61587618af07521900e267f10f5431
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-04-01 02:05:24 +0100

    melhorando ux

--- 13_crudredux/src/components/Products.jsx
+++ 13_crudredux/src/components/Products.jsx
@@ -17,9 +17,21 @@ const Products = () => {
   //obter estado
 
   const products = useSelector((state) => state.products.products);
+  const error = useSelector((state) => state.products.error);
+  const loading = useSelector((state) => state.products.loading);
   return (
     <Fragment>
       <h2 className="text-center my-5 ">Lista de produtos</h2>
+
+      {error ? (
+        <p className="font-weight-bold alert alert-danger text-center">
+          Houve um error
+        </p>
+      ) : null}
+      {loading ? (
+        <p className="text-center">
+         Loading...     </p>
+      ) : null}
 
       <table className="table table-striped">
         <thead className="bg-primary table-dark">
